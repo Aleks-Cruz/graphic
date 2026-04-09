173a5ce8f0b5505a8617b1b43ad6f2168f48e6cb
Aleks Emanuel Alcantara Cruz <873095@senacminas.edu.br>
2026-04-09 09:55:34 -0300

    Atualização

---
+++ robo.py
@@ -0,0 +1,20 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(661,459, duration=2)
+pyautogui.press("enter")
+sleep(3)
+pyautogui.click(965,616)
+pyautogui.write("caio123")
+pyautogui.press("enter")
+
+sleep(2)
+pyautogui.press("enter")
+
+
+
+
+
+
+
+
